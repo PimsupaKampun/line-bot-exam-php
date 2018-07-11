565b71709c8e25f4473d7b80ad44c26513e74d9b
PimsupaKampun <41123092+PimsupaKampun@users.noreply.github.com>
2018-07-12 00:07:34 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'fe/bI8lGY/X8JIfaW7DdwDeTc9oVwu858/m1PxZEBjk4/qdWlbMmiSaOYkKzpVYoiSanceBq+TqQ8OEB54kUZRcxAJlhetguCTQEK3+3HSgNayZAuBiWCOl/zzPXwInOTmExgubm6eGRU11L6Yx7JAdB04t89/1O/w1cDnyilFU==';
 
-$userId = 'Uce4bcee1688decbbfc116fea8982bb15';
+$userId = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
